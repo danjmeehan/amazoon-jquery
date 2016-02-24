edb29bbb1893c78a75f630529caba30d4e5dd21a
Dan Meehan <Dan Meehan>
2016-02-23 18:53:09 -0600

    preparing for publish

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,28 +1,75 @@
 $(document).ready(function(){
-    var products = [
-      {
-        name: "Amazon Fire TV",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product01.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 89.99
-      },
-      {
-        name: "Amazon Fire",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product02.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 69.00
-      },
-      {
-        name: "Amazon Echo",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product03.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 109.99
-      }
-    ];
 
-    // Get started here!
+  var products = $.get('http://portal.batchacademy.com/api/wdfne/amazoon-products', function(products){
+
+    console.log(products);
+
+
+      var updateView = function(){
+      // 1: Hide/show the .empty h3 tags based on the total products in the cart or the saved for later section
+      var productsInCart = $('.cart .product');
+      var productsSaved = $('.saved .product');
+
+      if(productsInCart.length === 0){
+        // Empty cart
+        $('.cart .empty').show();
+        $('.cart-total').hide();
+      } else {
+        // Products in cart
+        $('.cart .empty').hide();
+        $('.cart-total').show();
+      }
+
+      if(productsSaved.length === 0){
+        // No products saved
+        $('.saved .empty').show();
+      } else {
+        // Products saved
+        $('.saved .empty').hide();
+      }
+
+      // 2: Update the cart's total
+
+      var total = 0;
+      $(productsInCart).each(function(){
+        total += Number($(this).data('price'));
+      });
+
+      $('.cart-total span').text('$' + total.toFixed(2));
+
+    };
+
+      var productTemplate = $('template#product').html();
+   
+      for(var i = 0; i < products.length; i++){
+   
+        var newProduct = $(productTemplate);
+        var manufacturer = $('<span/>').addClass('manufacturer').text(' By ' + products[i].manufacturer);
+        var productData = products[i];
+
+        $('h2', newProduct).text(products[i].name).append(manufacturer);
+        $('img', newProduct).attr('src',(products[i].imageSrc));
+        $('h3', newProduct).text(products[i].description);
+        $('.price, .mobile-price', newProduct).text(' $ ' + products[i].price.toFixed(2));
+        $(newProduct).data('price',products[i].price);
+
+        $('.cart').append(newProduct);
+      }
+
+      updateView();
+
+      $('.move').on('click', function() {
+
+        if($(this).parents().eq(4).hasClass('cart')){
+          $(this).parents().eq(3).appendTo('.saved');
+          $(this).text('Add to cart');
+        } else {
+          $(this).parents().eq(3).appendTo('.cart');
+          $(this).text('Save for later');
+        }
+
+        updateView();
+      });
+  });
 
   });
--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-  // Get started here!
+  
+	$('form').on('submit', function(event){
+		event.preventDefault();
+
+
+	// Check that each required field is not empty
+
+	var requiredField = $('.form-control[required]');
+	var isValid = true;
+
+	$(requiredField).each(function(){
+		
+		// If a required field is empty then its parent should be red
+		if ($(this).val().length === 0) {
+			$(this).parent().addClass('has-error');
+			isValid = false;
+		
+		} else {
+			$(this).parent().removeClass('has-error');
+		}
+	});
+
+	if(isValid){
+		$('.error-message').hide();
+		alert('Form successfully submitted!')
+	} else {
+		$('.error-message').show();
+	}
+
+	});
+
 });
